e3599879fd8e20b6307bee3c2df08fa7128ed2c8
Anish Madhok <madhokanish@gmail.com>
2026-02-05 13:33:32 +0000

    Fix language switcher routes with rewrites

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const redirects = require("./src/data/redirects.json");
 
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/en/:path*",
+        destination: "/:path*"
+      },
+      {
+        source: "/hi/:path*",
+        destination: "/:path*"
+      }
+    ];
+  },
   async redirects() {
     return redirects.map((rule) => ({
       source: rule.source,
